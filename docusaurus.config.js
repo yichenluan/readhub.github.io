47510e124e4ed2fc0b920c41883bb34c2a959eb8
jin <hunter@jindeMacBook-Pro.local>
2023-04-12 22:10:12 +0800

    add 04.10

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ReadHub.ai',
-  tagline: 'read hub',
+  title: 'ReadHub',
+  tagline: 'Read Most Valuable AI Infos',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
